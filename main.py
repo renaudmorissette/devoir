d754038b6c780d9f77ee15b5f17fae55df55223e
6327663 <6327663@cstjean.qc.ca>
2025-02-05 09:19:02 -0500

    1

---
+++ main.py
@@ -0,0 +1,12 @@
+nom_dict = {"nom": "Renaud", "age": 18, "ville": "St-jean"}
+print(nom_dict["ville"])
+nom_dict["profession_future"] = "ingénieur"
+nom_dict["age"] = 20
+del nom_dict["ville"]
+etudiant = {"nom": "Gates", "prenom": "Bill", "age": 18, "notes": {"maths": 16, "français": 13, "chimie": 10}}
+
+print(etudiant["notes"]["chimie"])
+etudiant["anglais"] = 16
+for cle, valeur in etudiant["notes"].items():
+    print(f"La clé {cle} a pour valeur {valeur}")
+
