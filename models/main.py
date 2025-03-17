953b4f36ae1b07dd72b4f8e3d5739ccde561ee5f
6327663 <renaud.morissette@outlook.com>
2025-03-17 11:39:03 -0400

    groupe forme exercices

--- models/main.py
+++ models/main.py
@@ -6,25 +6,22 @@ def creer_rectangle():
 
     while True:
         try:
-            largeur=float(input("La largeur du rectangle: "))
-            if largeur<0:
+            largeur = float(input("La largeur du rectangle: "))
+            if largeur < 0:
                 print("La largeur du rectangle doit être positive")
                 continue
             break
         except ValueError:
             print("Veuillez entrez un nombre valide")
 
-
     while True:
         try:
-            longueur=float(input("La longueur du rectangle: "))
-            if longueur<0:
+            longueur = float(input("La longueur du rectangle: "))
+            if longueur < 0:
                 print("La longueur du rectangle doit être positive")
                 continue
             break
         except ValueError:
             print("Veuillez entrez un nombre valide")
 
-
-    return Rectangle(largeur,longueur)
-
+    return Rectangle(largeur, longueur)
